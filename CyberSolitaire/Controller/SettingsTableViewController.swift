1b208941245e225d7beaa0bccbfd714bc9ed323d
Christian Muth <christian-muth@t-online.de>
2018-10-09 09:58:41 +0200

    Fehler ist entdeckt! Bei der Rückkehr aus einer Popover oder Modal Segue darf man keine neue Segue benutzen sondern dismiss(... als nächstes müssen die Werte der Settings erfasst und ausgewertet werden.

--- CyberSolitaire/Controller/SettingsTableViewController.swift
+++ CyberSolitaire/Controller/SettingsTableViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class SettingsTableViewController: UITableViewController {
 
+    @IBAction func settingsAreDone(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
