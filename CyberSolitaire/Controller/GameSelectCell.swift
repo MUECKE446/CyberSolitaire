46deb33a6e65fd4d5b2aa2d797990febd57880f1
Christian Muth <christian-muth@t-online.de>
2018-10-23 15:48:39 +0200

    erster Versuch mit custom tableViewCell

---
+++ CyberSolitaire/Controller/GameSelectCell.swift
@@ -0,0 +1,51 @@
+//
+//  GameSelectCellTableViewCell.swift
+//  CyberSolitaire
+//
+//  Created by Christian Muth on 10.10.18.
+//  Copyright © 2018 Christian Muth. All rights reserved.
+//
+
+import UIKit
+
+class GameSelectCell: UITableViewCell {
+
+    @IBOutlet weak var gameNameLabel: UILabel!
+    @IBOutlet weak var disposalLabel: UILabel!
+    @IBOutlet weak var targetLabel: UILabel!
+    @IBOutlet weak var howToPlayLabel: UILabel!
+    
+//    override func awakeFromNib() {
+//        super.awakeFromNib()
+//        // Initialization code
+//    }
+//
+//    override func setSelected(_ selected: Bool, animated: Bool) {
+//        super.setSelected(selected, animated: animated)
+//
+//        // Configure the view for the selected state
+//    }
+
+//    override func systemLayoutSizeFitting(_ targetSize: CGSize, withHorizontalFittingPriority horizontalFittingPriority: UILayoutPriority, verticalFittingPriority: UILayoutPriority) -> CGSize {
+//        
+//        self.layoutIfNeeded()
+//        var size = super.systemLayoutSizeFitting(targetSize, withHorizontalFittingPriority: horizontalFittingPriority, verticalFittingPriority: verticalFittingPriority)
+//        
+//        if let textLabel = self.textLabel, let detailTextLabel = self.detailTextLabel {
+//            let detailHeight = detailTextLabel.frame.size.height
+//            if detailTextLabel.frame.origin.x > textLabel.frame.origin.x { // style = Value1 or Value2
+//                let textHeight = textLabel.frame.size.height
+//                if (detailHeight > textHeight) {
+//                    size.height += detailHeight - textHeight
+//                }
+//            } else { // style = Subtitle, so always add subtitle height
+//                size.height += detailHeight
+//            }
+//        }
+//        
+//        return size
+//        
+//    }
+    
+
+}
